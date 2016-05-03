4c72f472cfddeab11de69c90f2dca2d78d7950c4
Jason <853945995@qq.com>
2016-05-03 17:08:01 +0800

    5.3

---
+++ 新闻详情/新闻详情/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻详情
+//
+//  Created by czljcb on 16/3/29.
+//  Copyright © 2016年 czljcb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
